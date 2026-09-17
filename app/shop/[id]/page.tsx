87b830bae0d736e9002a2e219850724d1864f76b
LuckyHolo <keitama2530@gmail.com>
2026-09-17 15:57:19 +0900

    update

---
+++ app/shop/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { notFound } from "next/navigation"
+import { getShop } from "@/lib/services/shops"
+
+type Props = {
+    params: Promise<{ id: string }>
+}
+
+export default async function ShopDetailPage({ params }: Props) {
+    const { id } = await params
+    const shop = await getShop(id)
+
+    // Notfound 404
+    if (!shop) {
+        notFound()
+    }
+
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">{shop.name}</h1>
+        </div>
+    )
+}
